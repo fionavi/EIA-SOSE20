7b89724b6ffb07d8545788feb84c1cbee516a6fc
fionavi <fiona.noemi.virnich@hs-furtwangen.de>
2020-05-29 16:55:22 +0200

    Update script.js

--- Aufgabe 5/script.js
+++ Aufgabe 5/script.js
@@ -3,7 +3,7 @@ console.log("Europa");
 var emissionAbsolutEurope = 4209.3;
 var relativeEurope = 100 / (31116.5 / 4209.3);
 var growthEurope = (4209.3 - 4965.7) / 4965.7;
-var growthAbsoluteEurope = 4209.3 - 14965.7;
+var growthAbsoluteEurope = 4209.3 - 4965.7;
 console.log("Die Emission von Europa ist: " + emissionAbsolutEurope + "kg CO2");
 console.log("Relativ zur Gesamtemission der Welt verursacht Europa damit " + relativeEurope + "%");
 console.log("Für Europa hat sich 2018 im Vergleich zu 2008 die Emission um " + growthEurope + "% verändert");
