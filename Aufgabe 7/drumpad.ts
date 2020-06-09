452a3770fa07db7b7c8c60326f1ba52deec4b3ac
fionavi <fiona.noemi.virnich@hs-furtwangen.de>
2020-06-09 21:34:21 +0200

    ts pushen

--- Aufgabe 7/drumpad.ts
+++ Aufgabe 7/drumpad.ts
@@ -1,31 +1,22 @@
-var button1 = new Audio("https://raw.githubusercontent.com/gabriel-rausch/EIA1-SoSe20/master/L07/task_material/assets/hihat.mp3");
-button1.play();
-var button2: any = "kick";
-var button3: any = "snare";
-var button4: any = "A";
-var button5: any = "C";
-var button6: any = "F";
-var button7: any = "G";
-var button8: any = "laugh1";
-var button9: any = "laugh2";
+console.log("hallo welt");
 
+function playSample(): string {
+   var sounds: any [#audio1, #audio2, #audio3, #audio4, #audio5, #audio6, #audio7, #audio8, #audio9 ]
+   var audio: string = new Audio (sounds);
+    audio.play();
+  }
 
-
-
-function playSample(): void {
-
- button1.getElementById("#audio1").play();
- button2.getElementById("#audio2").play();
- button3.getElementById("#audio3").play();
- button4.getElementById("#audio4").play();
- button5.getElementById("#audio5").play();
- button6.getElementById("#audio6").play();
- button7.getElementById("#audio7").play();
- button8.getElementById("#audio8").play();
- button9.getElementById("#audio9").play();
-}
 
 window.addEventListener("load", function() {
 
-    document.querySelector(".button1")?.addEventListener("click", playSample);
-});
+document.querySelector(".button1").addEventListener("click", function () { playSample(0); });
+document.querySelector(".button2").addEventListener("click", function () { playSample(1); });
+document.querySelector(".button3").addEventListener("click", function () { playSample(2); });
+document.querySelector(".button4").addEventListener("click", function () { playSample(3); });
+document.querySelector(".button5").addEventListener("click", function () { playSample(4); });
+document.querySelector(".button6").addEventListener("click", function () { playSample(5); });
+document.querySelector(".button7").addEventListener("click", function () { playSample(6); });
+document.querySelector(".button8").addEventListener("click", function () { playSample(7); });
+document.querySelector(".button9").addEventListener("click", function () { playSample(8); });
+
+}
